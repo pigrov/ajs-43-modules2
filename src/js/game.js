a36761214aecb553607b621a1e8524e9e13583cd
Dmitry Pigrov <pigrov@gmail.com>
2022-05-30 22:23:06 +0300

    1

--- src/js/game.js
+++ src/js/game.js
@@ -2,15 +2,12 @@ import Character from './domain';
 
 export default class Game {
     start() {
-      console.log('game started');
+        console.log('game started');
     }
-  }
+}
   
-  export class GameSavingData {
-  }
+export class GameSavingData {}
   
-  export function readGameSaving() {
-  }
+export function readGameSaving() {}
   
-  export function writeGameSaving() {
-  }
+export function writeGameSaving() {}
